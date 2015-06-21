88468b32997f00a55f4abd98bd696c81a7055421
Janos Gyerik <janos@kronos>
2015-06-21 22:24:46 +0200

    remove leading non-numbers from matched inet line

--- functions.sh
+++ functions.sh
@@ -62,5 +62,5 @@ update_host() {
 }
 
 ip() {
-    ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}'
+    ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print $2}' | sed -e 's/^[^0-9]*//g'
 }
